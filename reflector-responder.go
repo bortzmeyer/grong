3ca3cce62661393d60a7edb324a76d8f13179025
Stephane Bortzmeyer <stephane+github@bortzmeyer.org>
2010-02-16 22:30:29 +0100

    Reflector responder now replies to A or AAAA queries

--- reflector-responder.go
+++ reflector-responder.go
@@ -1,3 +1,16 @@
+/* A name server which sends back the IP address of its client, the
+recursive resolver. When queried for type TXT, it sends back the text
+form of the address.  When queried for type A (resp. AAAA), it sends
+back the IPv4 (resp. v6) address.
+
+Similar services: whoami.ultradns.net, whoami.akamai.net. Also (but it
+is not their normal goal): rs.dns-oarc.net, porttest.dns-oarc.net,
+amiopen.openresolvers.org.
+
+Stephane Bortzmeyer <stephane+grong@bortzmeyer.org>
+
+*/
+
 package responder
 
 import (
@@ -5,9 +18,45 @@ import (
 	"./types"
 )
 
+// Compile-time options
+const includesPort = false // If false, sends only the address for TXT queries.
+// If true, includes the UDP or TCP port.
+// TODO: allow to secify the Qname it must respond to
+
 func txtRecord(client net.Addr) []byte {
 	sclient := client.String()
+	if !includesPort {
+		tcpAddr, _ := net.ResolveTCPAddr(sclient)
+		sclient = tcpAddr.IP.String()
+	}
 	return types.ToTXT(sclient)
+}
+
+func txtSection(qname string, client net.Addr) (result types.RR) {
+	result.Name = qname
+	result.Tipe = types.TXT
+	result.Class = types.IN
+	result.Ttl = 0
+	result.Data = txtRecord(client)
+	return
+}
+
+func addressSection(qname string, client net.IP) (result types.RR) {
+	result.Name = qname
+	result.Tipe = types.A
+	result.Class = types.IN
+	result.Ttl = 0
+	result.Data = client
+	return
+}
+
+func aaaaSection(qname string, client net.IP) (result types.RR) {
+	result.Name = qname
+	result.Tipe = types.AAAA
+	result.Class = types.IN
+	result.Ttl = 0
+	result.Data = client
+	return
 }
 
 func Respond(query types.DNSquery) types.DNSresponse {
@@ -15,30 +64,44 @@ func Respond(query types.DNSquery) types.DNSresponse {
 		result types.DNSresponse
 	)
 	result.Asection = nil
+	tcpAddr, _ := net.ResolveTCPAddr(query.Client.String())
+	ipaddressV4 := tcpAddr.IP.To4()
 	switch {
 	case query.Qclass != types.IN:
 		result.Responsecode = types.SERVFAIL
+	case query.Qtype == types.A:
+		result.Responsecode = types.NOERROR
+		if ipaddressV4 != nil {
+			ancount := 1
+			result.Asection = make([]types.RR, ancount)
+			result.Asection[0] = addressSection(query.Qname, ipaddressV4)
+		} else {
+			// ancount := 0
+		}
+	case query.Qtype == types.AAAA:
+		result.Responsecode = types.NOERROR
+		if ipaddressV4 == nil {
+			ancount := 1
+			result.Asection = make([]types.RR, ancount)
+			result.Asection[0] = aaaaSection(query.Qname, tcpAddr.IP)
+		} else {
+			// ancount := 0
+		}
 	case query.Qtype == types.TXT:
 		result.Responsecode = types.NOERROR
 		ancount := 1
 		result.Asection = make([]types.RR, ancount)
-		result.Asection[0].Name = query.Qname
-		result.Asection[0].Tipe = types.TXT
-		result.Asection[0].Class = types.IN
-		result.Asection[0].Ttl = 0
-		// TODO: better formatting, for instance allowing to have only the IP address
-		result.Asection[0].Data = txtRecord(query.Client)
+		result.Asection[0] = txtSection(query.Qname, query.Client)
 	case query.Qtype == types.ALL:
 		result.Responsecode = types.NOERROR
-		ancount := 1 // TODO: add an A or a AAAA
-		// TODO: reuse the code for the case of QTYPE=TXT
+		ancount := 2
 		result.Asection = make([]types.RR, ancount)
-		result.Asection[0].Name = query.Qname
-		result.Asection[0].Tipe = types.TXT
-		result.Asection[0].Class = types.IN
-		result.Asection[0].Ttl = 0
-		result.Asection[0].Data = txtRecord(query.Client)
-		// TODO: handle A and AAAA QTYPEs
+		result.Asection[0] = txtSection(query.Qname, query.Client)
+		if ipaddressV4 == nil {
+			result.Asection[1] = aaaaSection(query.Qname, tcpAddr.IP)
+		} else {
+			result.Asection[1] = addressSection(query.Qname, ipaddressV4)
+		}
 	case true:
 		result.Responsecode = types.NOERROR
 	}
@@ -46,4 +109,3 @@ func Respond(query types.DNSquery) types.DNSresponse {
 }
 
 func init() {}
-
